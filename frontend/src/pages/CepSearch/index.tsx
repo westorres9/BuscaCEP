b1cdad6641d58e7694801b52ec0779110c721d16
westorres9 <wester.torres9@gmail.com>
2022-07-05 14:11:03 -0300

    projeto buscaCEP

---
+++ frontend/src/pages/CepSearch/index.tsx
@@ -0,0 +1,80 @@
+import './styles.css';
+
+import ResultCard from 'components/ResultCard';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+type FormData = {
+  cep: string;
+}
+
+type Adderss = {
+  logradouro: string;
+  localidade: string;
+}
+
+const CepSearch = () => {
+
+  const [address, setAddress] = useState<Adderss>();
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: '',
+  });
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios.get(`https://viacep.com.br/ws/${formData.cep}/json`)
+    .then((response) => {
+      setAddress(response.data);
+      console.log(response.data)
+    })
+    .catch((error => {
+      setAddress(undefined);
+      console.log(error);
+    }));
+  }
+
+
+
+  return (
+    <div className="cep-search-container">
+      <h1 className="text-primary">Busca CEP</h1>
+      <div className="container search-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-container">
+            <input
+              type="text"
+              name="cep"
+              value={formData.cep}
+              className="search-input"
+              placeholder="CEP (somente números)"
+              onChange={handleChange}
+            />
+            <button type="submit" className="btn btn-primary search-button">
+              Buscar
+            </button>
+          </div>
+        </form>
+        {
+          address &&
+          <>
+            <ResultCard title="Logradouro" description={address?.logradouro} />
+            <ResultCard title="Número" description={address?.localidade} />
+          </>
+
+        }
+      </div>
+    </div>
+  );
+};
+
+export default CepSearch;
